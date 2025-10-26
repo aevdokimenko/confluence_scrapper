87adf5e87c2ef1d9c5ab70967feea56572a7aef1
Alex Evdokimenko <aevdokimenko@gmail.com>
2025-10-26 17:12:20 -0400

    Navigation update

--- scraper.py
+++ scraper.py
@@ -14,7 +14,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 class ConfluenceScraper:
-    def __init__(self, base_url: str = "https://confluence.veeam.com"):
+    def __init__(self, base_url: str):
         self.base_url = base_url
         self.session = requests.Session()
         self.session_cookie = None
